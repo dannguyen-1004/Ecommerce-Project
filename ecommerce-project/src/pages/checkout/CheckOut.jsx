bcba90fddbd62784cb631522505b8a6ed7f18a5e
Dan <nguyen.dan3@northeastern.edu>
2026-03-09 18:47:14 -0400

    Replace a with Link for CheckoutHeader

--- ecommerce-project/src/pages/checkout/CheckOut.jsx
+++ ecommerce-project/src/pages/checkout/CheckOut.jsx
@@ -1,5 +1,6 @@
 import "./checkout.css";
 import "./checkout-header.css";
+import { Link } from "react-router";
 
 export function CheckOut() {
   return (
@@ -7,17 +8,17 @@ export function CheckOut() {
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="/">
+            <Link to="/">
               <img className="logo" src="images/logo.png" />
               <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
+            </Link>
           </div>
 
           <div className="checkout-header-middle-section">
             Checkout (
-            <a className="return-to-home-link" href="/">
+            <Link className="return-to-home-link" to="/">
               3 items
-            </a>
+            </Link>
             )
           </div>
 
@@ -33,7 +34,9 @@ export function CheckOut() {
         <div className="checkout-grid">
           <div className="order-summary">
             <div className="cart-item-container">
-              <div className="delivery-date">Delivery date: Tuesday, June 21</div>
+              <div className="delivery-date">
+                Delivery date: Tuesday, June 21
+              </div>
 
               <div className="cart-item-details-grid">
                 <img
@@ -71,7 +74,9 @@ export function CheckOut() {
                       name="delivery-option-1"
                     />
                     <div>
-                      <div className="delivery-option-date">Tuesday, June 21</div>
+                      <div className="delivery-option-date">
+                        Tuesday, June 21
+                      </div>
                       <div className="delivery-option-price">FREE Shipping</div>
                     </div>
                   </div>
@@ -82,8 +87,12 @@ export function CheckOut() {
                       name="delivery-option-1"
                     />
                     <div>
-                      <div className="delivery-option-date">Wednesday, June 15</div>
-                      <div className="delivery-option-price">$4.99 - Shipping</div>
+                      <div className="delivery-option-date">
+                        Wednesday, June 15
+                      </div>
+                      <div className="delivery-option-price">
+                        $4.99 - Shipping
+                      </div>
                     </div>
                   </div>
                   <div className="delivery-option">
@@ -93,8 +102,12 @@ export function CheckOut() {
                       name="delivery-option-1"
                     />
                     <div>
-                      <div className="delivery-option-date">Monday, June 13</div>
-                      <div className="delivery-option-price">$9.99 - Shipping</div>
+                      <div className="delivery-option-date">
+                        Monday, June 13
+                      </div>
+                      <div className="delivery-option-price">
+                        $9.99 - Shipping
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -102,7 +115,9 @@ export function CheckOut() {
             </div>
 
             <div className="cart-item-container">
-              <div className="delivery-date">Delivery date: Wednesday, June 15</div>
+              <div className="delivery-date">
+                Delivery date: Wednesday, June 15
+              </div>
 
               <div className="cart-item-details-grid">
                 <img
@@ -111,7 +126,9 @@ export function CheckOut() {
                 />
 
                 <div className="cart-item-details">
-                  <div className="product-name">Intermediate Size Basketball</div>
+                  <div className="product-name">
+                    Intermediate Size Basketball
+                  </div>
                   <div className="product-price">$20.95</div>
                   <div className="product-quantity">
                     <span>
@@ -138,7 +155,9 @@ export function CheckOut() {
                       name="delivery-option-2"
                     />
                     <div>
-                      <div className="delivery-option-date">Tuesday, June 21</div>
+                      <div className="delivery-option-date">
+                        Tuesday, June 21
+                      </div>
                       <div className="delivery-option-price">FREE Shipping</div>
                     </div>
                   </div>
@@ -150,8 +169,12 @@ export function CheckOut() {
                       name="delivery-option-2"
                     />
                     <div>
-                      <div className="delivery-option-date">Wednesday, June 15</div>
-                      <div className="delivery-option-price">$4.99 - Shipping</div>
+                      <div className="delivery-option-date">
+                        Wednesday, June 15
+                      </div>
+                      <div className="delivery-option-price">
+                        $4.99 - Shipping
+                      </div>
                     </div>
                   </div>
                   <div className="delivery-option">
@@ -161,8 +184,12 @@ export function CheckOut() {
                       name="delivery-option-2"
                     />
                     <div>
-                      <div className="delivery-option-date">Monday, June 13</div>
-                      <div className="delivery-option-price">$9.99 - Shipping</div>
+                      <div className="delivery-option-date">
+                        Monday, June 13
+                      </div>
+                      <div className="delivery-option-price">
+                        $9.99 - Shipping
+                      </div>
                     </div>
                   </div>
                 </div>
