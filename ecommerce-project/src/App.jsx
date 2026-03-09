38d7c7bb215cda12988156c3b310e88076dc6ef8
Dan <nguyen.dan3@northeastern.edu>
2026-03-09 18:36:32 -0400

    Move header, pages (checkout, home, tracking, order) into react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
-import { HomePage } from "./pages/HomePage";
+import { CheckOut } from "./pages/checkout/CheckOut";
+import { HomePage } from "./pages/homepage/HomePage";
+import { Tracking } from "./pages/tracking/Tracking";
 import { Routes, Route } from "react-router";
+import { Orders } from "./pages/orders/Orders";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckOut />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tracking" element={<Tracking />} />
       </Routes>
     </>
   );
